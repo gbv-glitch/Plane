b86881b2c9b244886e308429f6b92751221bb2e1
gbv-glitch <gustav.vendelin@gmail.com>
2025-10-19 10:29:19 +0300

    Fixed camera system with helicopter scene

---
+++ Assets/Scripts/HeliControls.cs
@@ -0,0 +1,63 @@
+using System;
+using JetBrains.Annotations;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeliControls : MonoBehaviour
+{ 
+    //Kaamera "giidi" positioon
+    public Transform mainCameraGuidePosition;
+
+    //Lennuki positioon
+    public Transform playerPosition;
+
+    //Lennuki kiirus
+    public float thrust = 20;
+
+    //Lennuki pööramine
+    private float yaw;
+    private float pitch;
+    private float roll;
+    private float horizontalInput;
+    private float verticalInput;
+    private float horizontalInputYaw;
+
+    //Mängu sündmused toimuvad siin
+    private void Update()
+    {
+        //Liigutame  helikopteri üles
+        transform.position += transform.up * thrust * Time.deltaTime;//Viimane on seal, et kõigil oleks ükskõik mis arvutil sama kiirus
+
+        //Me siin registreerime, mida mängija tahab teha
+        horizontalInput = Input.GetAxis("Horizontal") * 3;
+        verticalInput = -(Input.GetAxis("Vertical") * 5);
+        horizontalInputYaw = Input.GetAxis("HorizontalYaw");
+
+        //Siin paneme info oma muutujasse
+        yaw += horizontalInputYaw * Time.deltaTime * 50;
+        pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * MathF.Sign(verticalInput * Time.deltaTime);
+        roll = Mathf.Lerp(0, 20, Mathf.Abs(horizontalInput)) * -MathF.Sign(horizontalInput * Time.deltaTime);
+
+        //Siin me pöörame oma mängijat
+        transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+
+        //Siin me toome kaamera mängija järel
+        mainCameraGuidePosition.position = playerPosition.position;
+
+        //Siin me pöörame oma kaamerat mängija järel
+        mainCameraGuidePosition.Rotate(0, horizontalInputYaw * Time.deltaTime * 50, 0);
+
+        //Siin me muudame mängija kiirust
+        thrust += Input.mouseScrollDelta.y;
+
+        //Selleks et mängija ei saaks kohal seista, kontrollime, kas lennukiirus on väiksem kui 15. Kui on, siis me paneme selle 15ks.
+        if (thrust <= -15)
+        {
+            thrust = -15;
+        }
+
+        print(thrust);
+    }
+}
